0fc98dccc45a9a8e9b2f1b7c9c8c19c4deca13fe
alintm4 <alintimilsana@gmail.com>
2026-08-26 16:32:44 +0545

    Add Mood NFT interactions and screenshots

--- Makefile
+++ Makefile
@@ -45,11 +45,11 @@ mint:
 deployMood:
 	@forge script script/DeployMoodNft.s.sol:DeployMoodNft $(NETWORK_ARGS)
 
-# mintMoodNft:
-# 	@forge script script/Interactions.s.sol:MintMoodNft $(NETWORK_ARGS)
+mintMood:
+	@forge script script/Interactions.s.sol:MintMoodNft $(NETWORK_ARGS)
 
-# flipMoodNft:
-# 	@forge script script/Interactions.s.sol:FlipMoodNft $(NETWORK_ARGS)
+flipMood:
+	@forge script script/Interactions.s.sol:FlipMoodNft $(NETWORK_ARGS)
 
 # zkdeploy: 
 # 	@forge create src/OurToken.sol:OurToken --rpc-url http://127.0.0.1:8011 --private-key $(DEFAULT_ZKSYNC_LOCAL_KEY) --legacy --zksync
--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,10 +5,11 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
+import {MoodNft} from "../src/MoodNft.sol";
 
 
 contract MintBasicNft is Script{
-    string constant public PUG="ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    string constant public PUG="data:application/json;base64,eyJuYW1lIjoiUFVHIiwiZGVzY3JpcHRpb24iOiJBbiBhZG9yYWJsZSBQVUcgcHVwISIsImltYWdlIjoiaHR0cHM6Ly9nYXRld2F5LnBpbmF0YS5jbG91ZC9pcGZzL1FtU3NZUngzTHBEQWIxR1pRbTd6WjFBdUhaamZiUGtENko3czlyNDF4dTFtZjg/ZmlsZW5hbWU9cHVnLnBuZyIsImF0dHJpYnV0ZXMiOlt7InRyYWl0X3R5cGUiOiJjdXRlbmVzcyIsInZhbHVlIjoxMDB9XX0=";
     function run() external{
         address mostRecentlyDeployed= DevOpsTools.get_most_recent_deployment(
             "BasicNft", block.chainid
@@ -22,4 +23,34 @@ contract MintBasicNft is Script{
         vm.stopBroadcast();
     }
 
+}
+
+contract MintMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNft", block.chainid
+        );
+        mintNftOnContract(mostRecentlyDeployed);
+    }
+
+    function mintNftOnContract(address contractAddress) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).mintNft();
+        vm.stopBroadcast();
+    }
+}
+
+contract FlipMoodNft is Script {
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNft", block.chainid
+        );
+        flipMoodOnContract(mostRecentlyDeployed, 0);
+    }
+
+    function flipMoodOnContract(address contractAddress, uint256 tokenId) public {
+        vm.startBroadcast();
+        MoodNft(contractAddress).flipMood(tokenId);
+        vm.stopBroadcast();
+    }
 }
